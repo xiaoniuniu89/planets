a0ce1a5db0a678c294a54aa89b4d8bc020e5c48e
Daniel Callaghan <dan_cal89@hotmail.com>
2022-01-19 17:01:57 +0000

    add script to handle active links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
 const hamburger = document.querySelector('.hamburger')
 const dropdown = document.querySelector('.nav-dropdown')
+const mTabs = document.querySelector('.mobile-tabs')
+const mTabItems = document.querySelectorAll('.mobile-tab-link')
 
 hamburger.addEventListener('click', () => {
     dropdown.classList.toggle('hide');
+    mTabs.classList.toggle('hide');
     hamburger.classList.toggle('change-grey')
 })
+
+// console.log(mTabItems)
+
+function clearActiveTab() {
+    mTabItems.forEach(mTabItem =>{
+        if(mTabItem.classList.contains('active')){
+            mTabItem.classList.remove('active')
+        }
+    })
+}
+
+
+
+mTabItems.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        clearActiveTab()
+        e.target.classList.toggle('active')
+    })
+})
+
