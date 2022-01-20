88883458235bab90f9cc7fa43298d8c58855e212
Daniel Callaghan <dan_cal89@hotmail.com>
2022-01-20 14:57:06 +0000

    add tablet responsivness and features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
+
+
 const hamburger = document.querySelector('.hamburger')
 const dropdown = document.querySelector('.nav-dropdown')
 const mTabs = document.querySelector('.mobile-tabs')
+const lTabs = document.querySelector('.tabs-lg')
+const lTabItems = document.querySelectorAll('.big-tab-link')
 const mTabItems = document.querySelectorAll('.mobile-tab-link')
 const lNavItems = document.querySelectorAll('.navlink-bg')
 const planetPic = document.querySelectorAll('.planet-pic')
+const planetTxt = document.querySelectorAll('.info-txt')
 const overviewPic = document.querySelector('.overview-pic')
 const surfacePic = document.querySelector('.surface-pic')
+const surfaceTxt = document.querySelector('.surface-txt')
 
 
 hamburger.addEventListener('click', () => {
@@ -22,6 +28,15 @@ function clearActiveTab() {
             mTabItem.classList.add('inactive')
         }
     })
+    
+}
+
+function clearActiveTabBig() {
+    lTabItems.forEach(lTabItem =>{
+        if(!lTabItem.classList.contains('inactive')){
+            lTabItem.classList.add('inactive')
+        }
+    })
 }
 
 function clearHidden() {
@@ -30,19 +45,30 @@ function clearHidden() {
             planetItem.classList.add('hide')
         }
     })
+    planetTxt.forEach(planetItem =>{
+        if(!planetItem.classList.contains('hide')){
+            planetItem.classList.add('hide')
+        }
+    })
 }
-
 function addDisplay(target) {
             planetPic.forEach(planetItem => {
                 if(target === 'surface'){
                     overviewPic.classList.toggle('hide');
                     surfacePic.classList.toggle('hide')
+                }else if(planetItem.classList.contains(target)){
+                    planetItem.classList.toggle('hide')
+                } 
+                
+            })
+            planetTxt.forEach(planetItem => {
+                if(target === 'surface'){
+                    surfaceTxt.classList.toggle('hide')
                 }else {
                     if(planetItem.classList.contains(target)){
                         planetItem.classList.toggle('hide')
                     }
-                }
-                
+                }     
             })
         
 }
@@ -60,5 +86,19 @@ mTabItems.forEach(tab => {
 })
 
 
-
+lTabItems.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        clearActiveTabBig()
+        clearHidden()
+        if(tab.classList.contains('overview')){
+            addDisplay('overview')
+        } else if(tab.classList.contains('structure')){
+            addDisplay('structure')
+        } else if(tab.classList.contains('surface')){
+            addDisplay('surface')
+        }
+        tab.classList.toggle('inactive')
+        console.log(e.target)
+    })
+})
 
